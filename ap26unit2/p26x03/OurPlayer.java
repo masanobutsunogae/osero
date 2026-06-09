2f6fa1fe2ebdc532ea61e4493f793f23460ab7e9
LeeChanhee <tony011203@gmail.com>
2026-06-09 14:24:22 +0900

    Update OurPlayer.java

--- ap26unit2/p26x03/OurPlayer.java
+++ ap26unit2/p26x03/OurPlayer.java
@@ -38,7 +38,6 @@ public class OurPlayer extends ap26.Player {
   record EvalResult(Move move, float score) {}
 
   // nps計算用
-  static final boolean ENABLE_NPS_COUNT = true;
   AtomicLong nodeCount = new AtomicLong();
   double seconds;
 
@@ -115,7 +114,7 @@ public class OurPlayer extends ap26.Player {
 
   /** α-β 探索の max 側。ルート (depth == 0) では並列探索を行う。 */
   float maxSearch(Board currentBoard, float alpha, float beta, int depth) {
-    if (ENABLE_NPS_COUNT) nodeCount.incrementAndGet();
+    nodeCount.incrementAndGet();
 
     if (isTerminal(currentBoard, depth)) {
       return this.eval.value(currentBoard);
@@ -123,7 +122,7 @@ public class OurPlayer extends ap26.Player {
 
     // 探索は常に黒視点なので、ここでは黒の合法手を生成
     List<Move> moves = currentBoard.findLegalMoves(BLACK);
-    moves = order(currentBoard, moves, BLACK, true);
+    moves = order(moves);
 
     if (depth == 0) {
       // 各合法手に対して並列で探索
@@ -181,14 +180,14 @@ public class OurPlayer extends ap26.Player {
 
   /** α-β 探索の min 側。白（= 相手）の手を生成する。 */
   float minSearch(Board currentBoard, float alpha, float beta, int depth) {
-    if (ENABLE_NPS_COUNT) nodeCount.incrementAndGet();
+    nodeCount.incrementAndGet();
 
     if (isTerminal(currentBoard, depth)) {
       return this.eval.value(currentBoard);
     }
 
     List<Move> moves = currentBoard.findLegalMoves(WHITE);
-    moves = order(currentBoard, moves, WHITE, false);
+    moves = order(moves);
 
     for (Move nextMove : moves) {
       Board nextBoard = currentBoard.placed(nextMove);
@@ -209,21 +208,10 @@ public class OurPlayer extends ap26.Player {
   }
 
   /** 探索する手順を並び替える。 同じ評価値の手が複数あったとき、毎回同じ手を選んで単調になるのを避ける。 */
-  List<Move> order(Board board, List<Move> moves, Color color, Boolean descending) {
-    List<Move> ordered = new ArrayList<>(moves);
-
-    ordered.sort((m1, m2) -> {
-      float v1 = eval.value(board.placed(m1.colored(color)));
-      float v2 = eval.value(board.placed(m2.colored(color)));
-
-      if(descending){
-        return Float.compare(v2, v1);
-      }else{
-        return Float.compare(v1, v2);
-      }
-    });
-
-    return ordered;
+  List<Move> order(List<Move> moves) {
+    List<Move> shuffled = new ArrayList<>(moves);
+    Collections.shuffle(shuffled);
+    return shuffled;
   }
 
   public long getNodeCount() {
